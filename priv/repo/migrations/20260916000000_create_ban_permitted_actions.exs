15c05f76787781e4be9ecf10f691fa9e61f79bed
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-09-16 16:29:31 -0300

    Old `granular ban` table stuff was migrated to the table ban_permitted_actions

--- priv/repo/migrations/20260916000000_create_ban_permitted_actions.exs
+++ priv/repo/migrations/20260916000000_create_ban_permitted_actions.exs
@@ -14,99 +14,9 @@ defmodule Philomena.Repo.Migrations.CreateBanPermittedActions do
     create constraint(:ban_permitted_actions, :num_nonnulls,
              check: "num_nonnulls(user_ban_id, subnet_ban_id, fingerprint_ban_id) = 1"
            )
-
-    alter table(:user_bans) do
-      remove :ban_upload_image
-      remove :ban_downvote_image
-      remove :ban_upvote_image
-      remove :ban_comment_images
-      remove :ban_post_forum
-      remove :ban_reply_forum
-      remove :ban_send_pm
-      remove :ban_api_key
-      remove :ban_create_filters
-      remove :ban_galleries
-      remove :ban_manage_tags
-      remove :ban_commissions
-    end
-
-    alter table(:subnet_bans) do
-      remove :ban_upload_image
-      remove :ban_downvote_image
-      remove :ban_upvote_image
-      remove :ban_comment_images
-      remove :ban_post_forum
-      remove :ban_reply_forum
-      remove :ban_send_pm
-      remove :ban_api_key
-      remove :ban_create_filters
-      remove :ban_galleries
-      remove :ban_manage_tags
-      remove :ban_commissions
-    end
-
-    alter table(:fingerprint_bans) do
-      remove :ban_upload_image
-      remove :ban_downvote_image
-      remove :ban_upvote_image
-      remove :ban_comment_images
-      remove :ban_post_forum
-      remove :ban_reply_forum
-      remove :ban_send_pm
-      remove :ban_api_key
-      remove :ban_create_filters
-      remove :ban_galleries
-      remove :ban_manage_tags
-      remove :ban_commissions
-    end
   end
 
   def down do
-    alter table(:user_bans) do
-      add :ban_upload_image, :boolean, default: false
-      add :ban_downvote_image, :boolean, default: false
-      add :ban_upvote_image, :boolean, default: false
-      add :ban_comment_images, :boolean, default: false
-      add :ban_post_forum, :boolean, default: false
-      add :ban_reply_forum, :boolean, default: false
-      add :ban_send_pm, :boolean, default: false
-      add :ban_api_key, :boolean, default: false
-      add :ban_create_filters, :boolean, default: false
-      add :ban_galleries, :boolean, default: false
-      add :ban_manage_tags, :boolean, default: false
-      add :ban_commissions, :boolean, default: false
-    end
-
-    alter table(:subnet_bans) do
-      add :ban_upload_image, :boolean, default: false
-      add :ban_downvote_image, :boolean, default: false
-      add :ban_upvote_image, :boolean, default: false
-      add :ban_comment_images, :boolean, default: false
-      add :ban_post_forum, :boolean, default: false
-      add :ban_reply_forum, :boolean, default: false
-      add :ban_send_pm, :boolean, default: false
-      add :ban_api_key, :boolean, default: false
-      add :ban_create_filters, :boolean, default: false
-      add :ban_galleries, :boolean, default: false
-      add :ban_manage_tags, :boolean, default: false
-      add :ban_commissions, :boolean, default: false
-    end
-
-    alter table(:fingerprint_bans) do
-      add :ban_upload_image, :boolean, default: false
-      add :ban_downvote_image, :boolean, default: false
-      add :ban_upvote_image, :boolean, default: false
-      add :ban_comment_images, :boolean, default: false
-      add :ban_post_forum, :boolean, default: false
-      add :ban_reply_forum, :boolean, default: false
-      add :ban_send_pm, :boolean, default: false
-      add :ban_api_key, :boolean, default: false
-      add :ban_create_filters, :boolean, default: false
-      add :ban_galleries, :boolean, default: false
-      add :ban_manage_tags, :boolean, default: false
-      add :ban_commissions, :boolean, default: false
-    end
-
     drop table(:ban_permitted_actions)
   end
 end
