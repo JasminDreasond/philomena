aa9384a4f9c971e8545ef6e7ed0090d60c563d6e
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-09-17 00:39:06 -0300

    old granular ban scripts have been disconnected temporarily (keeping the ban_view_helper working)

--- lib/philomena_web/plugs/api_require_authorization_plug.ex
+++ lib/philomena_web/plugs/api_require_authorization_plug.ex
@@ -25,13 +25,6 @@ defmodule PhilomenaWeb.ApiRequireAuthorizationPlug do
     |> maybe_forbidden(Bans.find(user, conn.remote_ip, "NOTAPI"))
   end
 
-  defp apply_ban_protocol(conn) do
-    conn
-    |> Conn.put_status(:forbidden)
-    |> Controller.text("")
-    |> Conn.halt()
-  end
-
   defp maybe_unauthorized(conn, nil) do
     conn
     |> Conn.put_status(:unauthorized)
@@ -43,28 +36,10 @@ defmodule PhilomenaWeb.ApiRequireAuthorizationPlug do
 
   defp maybe_forbidden(conn, nil), do: conn
 
-  defp maybe_forbidden(conn, bans) when is_list(bans) do
-    should_halt =
-      Enum.any?(bans, fn current_ban ->
-        not PhilomenaWeb.BanReasonHelper.any_granular_ban?(current_ban) or
-          matches_granular_ban?(current_ban, conn)
-      end)
-
-    if should_halt, do: apply_ban_protocol(conn), else: conn
-  end
-
-  defp maybe_forbidden(conn, current_ban) do
-    should_halt =
-      not PhilomenaWeb.BanReasonHelper.any_granular_ban?(current_ban) or
-        matches_granular_ban?(current_ban, conn)
-
-    if should_halt, do: apply_ban_protocol(conn), else: conn
-  end
-
-  defp matches_granular_ban?(current_ban, conn) do
-    case PhilomenaWeb.BanReasonHelper.get_current_request_reason(conn) do
-      nil -> false
-      reason -> PhilomenaWeb.BanReasonHelper.has_action?(current_ban, reason)
-    end
+  defp maybe_forbidden(conn, _current_ban) do
+    conn
+    |> Conn.put_status(:forbidden)
+    |> Controller.text("")
+    |> Conn.halt()
   end
 end
