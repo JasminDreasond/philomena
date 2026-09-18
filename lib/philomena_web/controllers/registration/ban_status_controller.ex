9425261de06f38fc593e0c46630e7eee09a5bd20
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-09-18 02:39:53 -0300

    Ban history added into Account Standing page

--- lib/philomena_web/controllers/registration/ban_status_controller.ex
+++ lib/philomena_web/controllers/registration/ban_status_controller.ex
@@ -1,14 +1,25 @@
 defmodule PhilomenaWeb.Registration.BanStatusController do
   use PhilomenaWeb, :controller
 
+  import Ecto.Query
+  alias Philomena.Repo
   alias PhilomenaWeb.BanReasonHelper
+  alias Philomena.Bans.User
+
+  defp user_bans(user) do
+    User
+    |> where(user_id: ^user.id)
+    |> order_by(desc: :created_at)
+    |> Repo.all()
+  end
 
   def show(conn, _params) do
     grouped_actions = BanReasonHelper.actions_grouped_by_category()
 
     render(conn, "show.html",
       title: "Account Standing",
-      grouped_actions: grouped_actions
+      grouped_actions: grouped_actions,
+      bans: user_bans(conn.assigns.current_user)
     )
   end
 end
